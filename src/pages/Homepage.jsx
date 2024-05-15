72cc25375aab3282f88edf95c9d1e3cb28d07600
alperentankut <tankut.alperen@gmail.com>
2024-05-15 21:38:54 +0300

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import HomepageHead from "../components/HomepageHead"
+import "../styles/homepage.css"
+
+const Homepage = () => {
+  return (
+    <div className="main-home">
+      <HomepageHead/>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+const path = require("path")
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  }
+})
