6e77950e28dd18260e4bbd299f94c4520f6e203a
alperentankut <tankut.alperen@gmail.com>
2024-05-15 23:15:05 +0300

    update

--- src/components/HomepageHead.jsx
+++ src/components/HomepageHead.jsx
@@ -1,13 +1,13 @@
-import Rectangle from "../assets/Rectangle 28.png";
-import { useState } from "react";
+
+// import { useState } from "react";
 const HomepageHead = () => {
-  const [isHovered, setIsHovered] = useState(false);
+//   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="main-container d-flex flex-column align-items-center">
+    <div className="main-container d-flex flex-column align-items-center ">
       <div className="ali-choubin-ebaotrc-cnq-4-unsplash-1 d-flex flex-column align-items-center">
         <div className="rectangle-28">
-          <img src={Rectangle}></img>
+          
           <div className="vector">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -39,10 +39,12 @@ const HomepageHead = () => {
         </div>
         <div
           className="frame-1"
-          onClick={() => setIsHovered(true)}
+        //   onClick={() => setIsHovered(true)}
          
         >
-          <div className="anasayfa">Anasayfa</div>
+          <div className="anasayfa">Anasayfa
+          </div>
+          
 
           <div className="hakkmzda">Hakkımızda</div>
           <div className="rnlerimiz">Ürünlerimiz</div>
@@ -70,53 +72,7 @@ const HomepageHead = () => {
             </div>
           </div>
         </div>
-        {isHovered && (
-          <div className="frame-4">
-            <svg
-              className="line-2-stroke"
-              xmlns="http://www.w3.org/2000/svg"
-              width="46"
-              height="2"
-              viewBox="0 0 46 2"
-              fill="none"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M45.5476 1.09399H0V0.0939941H45.5476V1.09399Z"
-                fill="#FFCD00"
-              />
-            </svg>
-            <svg
-              className="vector-select"
-              xmlns="http://www.w3.org/2000/svg"
-              width="19"
-              height="7"
-              viewBox="0 0 19 7"
-              fill="none"
-            >
-              <path
-                d="M6.09764 4.62863C4.92881 4.62863 4.04568 5.49443 3.8985 6.59399H0.547852C0.894172 3.85806 2.95477 0.593994 6.24481 0.593994C7.37901 0.593994 8.72101 1.06153 9.9591 1.49443C11.1972 1.96196 12.3314 2.42083 13.0327 2.42083C14.0976 2.42083 14.9375 1.52906 15.0933 0.593994H18.4526C18.0284 3.6243 16.4699 6.2217 12.8249 6.2217C10.0976 6.2217 7.65608 4.62863 6.09764 4.62863Z"
-                fill="#FFCC07"
-              />
-            </svg>
-            <svg
-              className="line-3-stroke"
-              xmlns="http://www.w3.org/2000/svg"
-              width="46"
-              height="2"
-              viewBox="0 0 46 2"
-              fill="none"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M45.9998 1.09399H0.452148V0.0939941H45.9998V1.09399Z"
-                fill="#FFCD00"
-              />
-            </svg>
-          </div>
-        )}
+        
         <div className="dlednzevn ">DÜŞLEDİĞİNİZ EVİN</div>
         <div className="vazgeemeyecenzdetayi">VAZGEÇEMEYECEĞİNİZ DETAYI</div>
       </div>
